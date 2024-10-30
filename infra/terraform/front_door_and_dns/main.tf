d51a239ed95b5564f64204c173af7198bfa34871
Tim Reilly <timreilly@live.com>
2024-10-30 00:48:03 +0000

    domain clash

--- infra/terraform/front_door_and_dns/main.tf
+++ infra/terraform/front_door_and_dns/main.tf
@@ -24,6 +24,255 @@ resource "azurerm_dns_zone" "dnszones_timmyreilly_com" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
+# Create Azure Front Door Profile
+resource "azurerm_cdn_frontdoor_profile" "prodFrontDoor" {
+  name                = var.profiles_prodFrontDoor_name
+  resource_group_name = azurerm_resource_group.main.name
+  sku_name            = "Standard_AzureFrontDoor" # Use "Premium" or "Standard"
+
+  tags = {
+    env = "prod"
+  }
+}
+
+# Create Azure Front Door Endpoint
+resource "azurerm_cdn_frontdoor_endpoint" "trei_home" {
+  name                      = "trei-home"
+  cdn_frontdoor_profile_id  = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
+  enabled                   = true
+}
+
+resource "azurerm_cdn_frontdoor_endpoint" "afd_endpoint" {
+  name                = "timmyreilly-afd-endpoint"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
+  enabled                   = true
+}
+
+
+
+resource "azurerm_cdn_frontdoor_custom_domain" "root_custom_domain" {
+  name                     = "timmyreilly-com"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
+  dns_zone_id              = azurerm_dns_zone.dnszones_timmyreilly_com.id
+  host_name                = join(".", ["timmyreilly", azurerm_dns_zone.dnszones_timmyreilly_com.name])
+
+  tls {
+    certificate_type    = "ManagedCertificate"
+    minimum_tls_version = "TLS12"
+  }
+}
+
+
+resource "azurerm_cdn_frontdoor_custom_domain" "moolah_timmyreilly_com" {
+  name                      = "moolah-timmyreilly-com"
+  cdn_frontdoor_profile_id  = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
+  host_name                 = "moolahb.timmyreilly.com"
+
+  tls {
+    certificate_type = "ManagedCertificate"
+    minimum_tls_version = "TLS12"
+  }
+}
+
+# # Create DNS TXT Records for Domain Validation
+# # Example for iot.timmyreilly.com
+resource "azurerm_dns_txt_record" "moolah_timmyreilly_com_validation" {
+  name                = azurerm_cdn_frontdoor_custom_domain.moolah_timmyreilly_com.name
+  zone_name           = azurerm_dns_zone.dnszones_timmyreilly_com.name
+  resource_group_name = azurerm_resource_group.main.name
+  ttl                 = 3600
+
+  record {
+    value = azurerm_cdn_frontdoor_custom_domain.moolah_timmyreilly_com.validation_token
+  }
+}
+
+
+# # Create Origin Groups
+resource "azurerm_cdn_frontdoor_origin_group" "default_origin_group" {
+  name                      = "default-origin-group"
+  cdn_frontdoor_profile_id  = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
+
+  load_balancing {
+    sample_size                     = 4
+    successful_samples_required     = 3
+  }
+
+  health_probe {
+    path                    = "/"
+    protocol                = "Http"
+    interval_in_seconds     = 100
+    request_type            = "HEAD"
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "storage_origin_group" {
+  name                      = "storageorigingroup"
+  cdn_frontdoor_profile_id  = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
+
+  load_balancing {
+    sample_size                     = 4
+    successful_samples_required     = 3
+  }
+
+  health_probe {
+    path                    = "/"
+    protocol                = "Http"
+    interval_in_seconds     = 255
+    request_type            = "HEAD"
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "example" {
+  name                = "storage-origin-group"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
+
+  load_balancing {
+    additional_latency_in_milliseconds = 0
+    sample_size                        = 16
+    successful_samples_required        = 3
+  }
+}
+
+data "azurerm_storage_account" "storage" {
+  name                = "webstorage1february"
+  resource_group_name = "WebHome1"
+}
+
+resource "azurerm_cdn_frontdoor_profile" "afd_profile" {
+  name                = "beginner-profile"
+  resource_group_name = azurerm_resource_group.main.name
+  sku_name            = "Standard_AzureFrontDoor"
+}
+
+resource "azurerm_cdn_frontdoor_origin" "afd_origin" {
+  name                          = "storage-origin"
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.storage_origin_group.id
+  enabled                       = true
+
+  certificate_name_check_enabled = false
+
+  host_name          = data.azurerm_storage_account.storage.primary_web_host
+  http_port          = 80
+  https_port         = 443
+  origin_host_header = data.azurerm_storage_account.storage.primary_web_host
+  priority           = 1
+  weight             = 1
+}
+
+
+
+resource "azurerm_cdn_frontdoor_origin" "default_origin" {
+  name                          = "default-origin"
+  host_name                     = "webstorage1february.blob.core.windows.net"
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.default_origin_group.id
+  http_port                     = 80
+  https_port                    = 443
+  origin_host_header            = "webstorage1february.web.core.windows.net"
+  priority                      = 1
+  weight                        = 1000
+  enabled                       = true
+  certificate_name_check_enabled = true
+}
+
+resource "azurerm_cdn_frontdoor_origin" "staticwebappstorage" {
+  name                          = "staticwebappstorage"
+  host_name                     = "webstorage1february.z5.web.core.windows.net"
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.storage_origin_group.id
+  http_port                     = 80
+  https_port                    = 443
+  origin_host_header            = "webstorage1february.z5.web.core.windows.net"
+  priority                      = 1
+  weight                        = 1000
+  enabled                       = true
+  certificate_name_check_enabled = true
+}
+
+# # Create Routes
+resource "azurerm_cdn_frontdoor_route" "default_route" {
+  name                          = "default-route"
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.trei_home.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.storage_origin_group.id
+  supported_protocols           = ["Http", "Https"]
+  patterns_to_match             = ["/*"]
+  forwarding_protocol           = "MatchRequest"
+  https_redirect_enabled        = true
+  cdn_frontdoor_origin_ids      = []
+}
+
+resource "azurerm_cdn_frontdoor_rule_set" "beginner_rule_set" {
+  name                     = "BeginnerRuleSet"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.afd_profile.id
+}
+
+# Route to Redirect timmyreilly.com to www.timmyreilly.com
+resource "azurerm_cdn_frontdoor_route" "afd_route_www" {
+  name                          = "route-www"
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.trei_home.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.storage_origin_group.id
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.afd_origin.id]
+  cdn_frontdoor_rule_set_ids    = [azurerm_cdn_frontdoor_rule_set.beginner_rule_set.id]
+  enabled                       = true
+
+  forwarding_protocol    = "HttpsOnly"
+  https_redirect_enabled = true
+  patterns_to_match      = ["/*"]
+  supported_protocols    = ["Http", "Https"]
+
+  cdn_frontdoor_custom_domain_ids = [azurerm_cdn_frontdoor_custom_domain.moolah_timmyreilly_com.id, azurerm_cdn_frontdoor_custom_domain.root_custom_domain.id]
+  link_to_default_domain          = false
+
+  cache {
+    query_string_caching_behavior = "IgnoreSpecifiedQueryStrings"
+    query_strings                 = ["account", "settings"]
+    compression_enabled           = true
+    content_types_to_compress     = ["text/html", "text/javascript", "text/xml"]
+  }
+}
+
+
+# DNS CNAME Record for www.timmyreilly.com pointing to Front Door Endpoint
+resource "azurerm_dns_cname_record" "cname_record_www" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.dnszones_timmyreilly_com.name
+  resource_group_name = azurerm_resource_group.main.name
+  ttl                 = 300
+  record              = azurerm_cdn_frontdoor_endpoint.afd_endpoint.host_name
+}
+
+# DNS Alias A Record for timmyreilly.com pointing to Front Door Custom Domain
+# resource "azurerm_dns_a_record" "a_record_root_b" {
+#   name                = "@"
+#   zone_name           = azurerm_dns_zone.dnszones_timmyreilly_com.name
+#   resource_group_name = azurerm_resource_group.main.name
+
+#   ttl = 300
+
+#   target_resource_id = azurerm_cdn_frontdoor_custom_domain.root_custom_domain.id
+# }
+
+
+# # Create DNS Records
+resource "azurerm_dns_a_record" "a_record_root" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.dnszones_timmyreilly_com.name
+  resource_group_name = azurerm_resource_group.main.name
+  ttl                 = 3600
+
+  target_resource_id = azurerm_cdn_frontdoor_endpoint.trei_home.id
+}
+
+resource "azurerm_dns_cname_record" "cname_record_moolah" {
+  name                = "moolah"
+  zone_name           = azurerm_dns_zone.dnszones_timmyreilly_com.name
+  resource_group_name = azurerm_resource_group.main.name
+  ttl                 = 3600
+  record              = azurerm_cdn_frontdoor_endpoint.trei_home.host_name
+}
+
+
+### Archive of previous resources
+
 # # Create App Service Domain
 # resource "azurerm_app_service_domain" "domains_timmyreilly_com" {
 #   name                = var.domains_timmyreilly_com_name
@@ -83,23 +332,35 @@ resource "azurerm_dns_zone" "dnszones_timmyreilly_com" {
 #   tags       = {}
 # }
 
-# Create Azure Front Door Profile
-resource "azurerm_cdn_frontdoor_profile" "prodFrontDoor" {
-  name                = var.profiles_prodFrontDoor_name
-  resource_group_name = azurerm_resource_group.main.name
-  sku_name            = "Standard_AzureFrontDoor" # Use "Premium" or "Standard"
 
-  tags = {
-    env = "prod"
-  }
-}
+# resource "azurerm_cdn_frontdoor_custom_domain" "www_custom_domain" {
+#   name                     = "www-timmyreilly-com"
+#   resource_group_name      = azurerm_resource_group.main.name
+#   profile_name             = azurerm_cdn_frontdoor_profile.prodFrontDoor.name
+#   host_name                = "www.timmyreilly.com"
+#   dns_zone_id              = azurerm_dns_zone.dnszones_timmyreilly_com.id
 
-# Create Azure Front Door Endpoint
-resource "azurerm_cdn_frontdoor_endpoint" "trei_home" {
-  name                      = "trei-home"
-  cdn_frontdoor_profile_id  = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
-  enabled                   = false
-}
+#   # Enable HTTPS with Azure-managed certificates
+#   tls {
+#     certificate_type    = "ManagedCertificate"
+#     minimum_tls_version = "TLS12"
+#   }
+# }
+
+# resource "azurerm_cdn_frontdoor_custom_domain" "root_custom_domain" {
+#   name                     = "timmyreilly-com"
+#   resource_group_name      = azurerm_resource_group.main.name
+#   profile_name             = azurerm_cdn_frontdoor_profile.prodFrontDoor.name
+#   host_name                = "timmyreilly.com"
+#   dns_zone_id              = azurerm_dns_zone.dnszones_timmyreilly_com.id
+
+#   # Enable HTTPS with Azure-managed certificates
+#   tls {
+#     certificate_type    = "ManagedCertificate"
+#     minimum_tls_version = "TLS12"
+#   }
+# }
+
 
 # # Create Azure Front Door Custom Domains
 # resource "azurerm_cdn_frontdoor_custom_domain" "iot_timmyreilly_com" {
@@ -124,69 +385,23 @@ resource "azurerm_cdn_frontdoor_endpoint" "trei_home" {
 #   }
 # }
 
-resource "azurerm_cdn_frontdoor_custom_domain" "moolah_timmyreilly_com" {
-  name                      = "moolah-timmyreilly-com"
-  cdn_frontdoor_profile_id  = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
-  host_name                 = "moolahb.timmyreilly.com"
 
-  tls {
-    certificate_type = "ManagedCertificate"
-    minimum_tls_version = "TLS12"
-  }
-}
 
-# # Create DNS TXT Records for Domain Validation
-# # Example for iot.timmyreilly.com
-resource "azurerm_dns_txt_record" "moolah_timmyreilly_com_validation" {
-  name                = azurerm_cdn_frontdoor_custom_domain.moolah_timmyreilly_com.name
-  zone_name           = azurerm_dns_zone.dnszones_timmyreilly_com.name
-  resource_group_name = azurerm_resource_group.main.name
-  ttl                 = 3600
 
-  record {
-    value = azurerm_cdn_frontdoor_custom_domain.moolah_timmyreilly_com.validation_token
-  }
-}
 
-# # Include other domains as needed
-# # ...
 
-# # Create Origin Groups
-resource "azurerm_cdn_frontdoor_origin_group" "default_origin_group" {
-  name                      = "default-origin-group"
-  cdn_frontdoor_profile_id  = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
 
-  load_balancing {
-    # additional_latency_milliseconds = 50
-    sample_size                     = 4
-    successful_samples_required     = 3
-  }
 
-  health_probe {
-    path                    = "/"
-    protocol                = "Http"
-    interval_in_seconds     = 100
-    request_type            = "HEAD"
-  }
-}
+# resource "azurerm_cdn_frontdoor_origin_group" "afd_origin_group" {
+#   name                = "storage-origin-group"
+#   profile_name        = azurerm_cdn_frontdoor_profile.prodFrontDoor.name
+#   resource_group_name = azurerm_resource_group.main.name
 
-resource "azurerm_cdn_frontdoor_origin_group" "storage_origin_group" {
-  name                      = "storageorigingroup"
-  cdn_frontdoor_profile_id  = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
-
-  load_balancing {
-    # additional_latency_milliseconds = 50
-    sample_size                     = 4
-    successful_samples_required     = 3
-  }
-
-  health_probe {
-    path                    = "/"
-    protocol                = "Http"
-    interval_in_seconds     = 255
-    request_type            = "HEAD"
-  }
-}
+#   health_probe_enabled = true
+#   health_probe_method  = "HEAD"
+#   health_probe_path    = "/"
+#   probe_request_type   = "Https"
+# }
 
 # resource "azurerm_cdn_frontdoor_origin_group" "timmyreillyazurewebsitesnet" {
 #   name                      = "timmyreillyazurewebsitesnet"
@@ -220,46 +435,28 @@ resource "azurerm_cdn_frontdoor_origin_group" "storage_origin_group" {
 #   certificate_name_check_enabled = true
 # }
 
-resource "azurerm_cdn_frontdoor_origin" "default_origin" {
-  name                          = "default-origin"
-  host_name                     = "webstorage1february.blob.core.windows.net"
-  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.default_origin_group.id
-  http_port                     = 80
-  https_port                    = 443
-  origin_host_header            = "webstorage1february.web.core.windows.net"
-  priority                      = 1
-  weight                        = 1000
-  enabled                       = true
-  certificate_name_check_enabled = true
-}
 
-resource "azurerm_cdn_frontdoor_origin" "staticwebappstorage" {
-  name                          = "staticwebappstorage"
-  host_name                     = "webstorage1february.z5.web.core.windows.net"
-  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.storage_origin_group.id
-  http_port                     = 80
-  https_port                    = 443
-  origin_host_header            = "webstorage1february.z5.web.core.windows.net"
-  priority                      = 1
-  weight                        = 1000
-  enabled                       = true
-  certificate_name_check_enabled = true
-}
 
-# # Create Routes
-resource "azurerm_cdn_frontdoor_route" "default_route" {
-  name                          = "default-route"
-  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.trei_home.id
-  # cdn_frontdoor_profile_id      = azurerm_cdn_frontdoor_profile.prodFrontDoor.id
-  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.storage_origin_group.id
-  supported_protocols           = ["Http", "Https"]
-  patterns_to_match             = ["/*"]
-  forwarding_protocol           = "MatchRequest"
-  https_redirect_enabled        = true
-  cdn_frontdoor_origin_ids      = []
-  # custom_domains                = [azurerm_cdn_frontdoor_custom_domain.moolah_timmyreilly_com.id]
-  # enable_caching                = false
-}
+
+# resource "azurerm_cdn_frontdoor_origin" "afd_origin" {
+#   name                = "storage-origin"
+#   profile_name        = azurerm_cdn_frontdoor_profile.afd_profile.name
+#   # resource_group_name = azurerm_resource_group.rg.name
+#   # origin_group_name   = azurerm_cdn_frontdoor_origin_group.afd_origin_group.name
+
+#   host_name          = data.azurerm_storage_account.storage.primary_web_host
+#   origin_host_header = data.azurerm_storage_account.storage.primary_web_host
+#   http_port          = 80
+#   https_port         = 443
+#   priority           = 1
+#   weight             = 1000
+#   enabled            = true
+# }
+
+
+
+
+
 
 # resource "azurerm_cdn_frontdoor_route" "private_network_routing" {
 #   name                          = "private-network-routing"
@@ -274,23 +471,6 @@ resource "azurerm_cdn_frontdoor_route" "default_route" {
 #   # enable_caching                = false
 # }
 
-# # Create DNS Records
-resource "azurerm_dns_a_record" "a_record_root" {
-  name                = "@"
-  zone_name           = azurerm_dns_zone.dnszones_timmyreilly_com.name
-  resource_group_name = azurerm_resource_group.main.name
-  ttl                 = 3600
-
-  target_resource_id = azurerm_cdn_frontdoor_endpoint.trei_home.id
-}
-
-resource "azurerm_dns_cname_record" "cname_record_moolah" {
-  name                = "moolah"
-  zone_name           = azurerm_dns_zone.dnszones_timmyreilly_com.name
-  resource_group_name = azurerm_resource_group.main.name
-  ttl                 = 3600
-  record              = azurerm_cdn_frontdoor_endpoint.trei_home.host_name
-}
 
 # resource "azurerm_dns_txt_record" "txt_record_dnsauth" {
 #   name                = "_dnsauth"
